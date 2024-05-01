ae5a1db0d6e6f05ae26a36e08ed340a99aedd6ff
ALEESHA20 <164679957+ALEESHA20@users.noreply.github.com>
2024-05-01 10:18:36 -0700

    test repository

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let greet = "welcome";
+console.log(greet);
+let welcome = "alisha ";
+console.log(greet + " " + welcome);
---
+++ index.ts
@@ -0,0 +1,5 @@
+let greet = "welcome";
+console.log(greet);
+
+let welcome = "alisha ";
+console.log(greet + " " + welcome);
